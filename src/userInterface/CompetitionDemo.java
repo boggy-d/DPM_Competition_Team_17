43695f16b5a9f3e81f0705e1f389bc644c98f25c
Bogdan Dumitru <bogdan.dumitru@mail.mcgill.ca>
2016-12-02 03:23:54 -0500

    Javadocs done

---
+++ src/userInterface/CompetitionDemo.java
@@ -0,0 +1,53 @@
+/**
+ * The CompetitionDemo class contains the main method of the 
+ * robot's program. It prints the display onto the robot's
+ * LCD screen and checks if the Enter button has been pressed
+ * before giving control to the ActionController class
+ * 
+ *  @author Bogdan Dumitru
+ */
+
+
+package userInterface;
+
+import component.ActionController;
+import component.Constants;
+import lejos.hardware.Button;
+
+public class CompetitionDemo {
+
+	/**
+	 * Main Class of the program. Waits for the Enter button to be 
+	 * pressed and then loops the routine
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		isEnter();
+		ActionController ac = new ActionController();
+		
+		while(true)
+		{
+			ac.doRoutine();
+		}
+		
+	}
+	
+	
+	/**
+	 * Loops until either the Enter or Escape button is pressed
+	 * on the EV3 Brick. Pressing the Escape button will cause
+	 * the program to terminate
+	 */
+	public static void isEnter()
+	{
+		int buttonChoice = Button.waitForAnyPress();
+		
+		if(buttonChoice == Button.ID_ESCAPE)
+		{
+			System.exit(0);
+		}
+		else{
+			return;
+		}
+	}
+}
